9710a0cd582b1718d9bb32c14efa0258f045b09d
Mauro Nievas <54127211+Mauro-jpg@users.noreply.github.com>
2022-06-14 20:05:18 -0300

    scene 3D

--- components/cards.js
+++ components/cards.js
@@ -4,10 +4,12 @@ const Cards = () => {
 
 
     return <div  className=" w-10/12">
-    <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+    <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden ">
         <div className="p-6 ease-in">
-            <h1 className="text-2xl font-semibold mb-3">
-                Hola mundo
+            <h1 className="text-2xl font-semibold mb-3 text-slate-50">
+                As a young computing enthusiast, I studied programming in a self-taught way since I was 12 years old, I am currently studying computer 
+                engineering and I have experience with various technologies, I've been with Javascript and Python for almost 5 years, and I hav developed 
+                for both backend and frontend, I am looking for new technological challenges to grow as a developer
             </h1>
         </div>
     </div>
