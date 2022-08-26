4580e40616175dfa96f69a11e18602423de2e3a9
MauroJs <niev.mauro@gmail.com>
2022-08-25 21:09:16 -0300

    algunos cambios

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
-import { Container, Box, Heading, Image } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import { 
+    Container, 
+    Box,
+    Link,
+    Heading, 
+    Image,
+    Button,
+    useColorModeValue 
+} from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
     return  <Container>
-                <Box borderRadius="lg" bg="red" p={3} mb={6} align="center" >
+                <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500','whiteAlpha.200')} p={3} mb={6} align="center" >
                     Hello I&apos;m a full-stack developer based in Uruguay!
                 </Box>
                 <Box display={{md:'flex'}}>
@@ -25,6 +38,35 @@ const Page = () => {
                             />
                     </Box>
                 </Box>
+                <Section delay={0.1}>
+                    <Heading as="h3" variant="section-title">
+                        Work
+                    </Heading>
+                    <Paragraph>As a young computing enthusiast, I studied programming in a 
+                        selft-taught way since I was 12 years old,
+                        I am currently studying computer engineering and I have experience with
+                        various technologies, I've been with Javascript and Python for almost 5
+                        years, and I have developed for both backend and frontend,
+                        I am looking for new technological challenges to grow as a developer &nbsp;
+                        <NextLink href="/work/inkdrop"><Link>Inkdrop</Link></NextLink>.
+                    </Paragraph>
+                    <Box align="center" my={4}>
+                        <NextLink href="/works">
+                            <Button righIcon={<ChevronRightIcon/>} colorScheme="teal">
+                                My porfolio
+                            </Button>
+                        </NextLink>
+                    </Box>
+                </Section>
+                <Section delay={0.2}>
+                    <Heading as="h3" variant="section-title">
+                        Bio
+                    </Heading>
+                    <BioSection>
+                        <BioYear>1996</BioYear>
+                        Born in Mercedes, Uruguay.
+                    </BioSection>
+                </Section>
             </Container> 
 }
 
