f8f65cfe7e27388e2481f9e1963ed3a14529e3e8
MauroJs <niev.mauro@gmail.com>
2022-09-11 19:45:27 -0300

    Layouts

--- pages/index.js
+++ pages/index.js
@@ -9,65 +9,68 @@ import {
     useColorModeValue 
 } from '@chakra-ui/react'
 import { ChevronRightIcon } from '@chakra-ui/icons'
+import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
 import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
-    return  <Container>
-                <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500','whiteAlpha.200')} p={3} mb={6} align="center" >
-                    Hello I&apos;m a full-stack developer based in Uruguay!
-                </Box>
-                <Box display={{md:'flex'}}>
-                    <Box flexGrow={1}>
-                        <Heading as="h2" variant="page-title" >
-                             Mauro Nievas
+    return  <Layout>
+                <Container>
+                    <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500','whiteAlpha.200')} p={3} mb={6} align="center" >
+                        Hello I&apos;m a full-stack developer based in Uruguay!
+                    </Box>
+                    <Box display={{md:'flex'}}>
+                        <Box flexGrow={1}>
+                            <Heading as="h2" variant="page-title" >
+                                Mauro Nievas
+                            </Heading>
+                            <p>Full-stack developer</p>
+                        </Box>
+                        <Box flexShrink={0} mt={{base:4, md:0}} ml={{md:6}} align="center">
+                            <Image 
+                                borderColor="whiteAlpha.800" 
+                                borderWidth={2} 
+                                borderStyle="solid" 
+                                maxWidth="100px" 
+                                display="inline-block" 
+                                borderRadius="full" 
+                                src="/images/mauro.jpeg" 
+                                alt="Profile Image"
+                                />
+                        </Box>
+                    </Box>
+                    <Section delay={0.1}>
+                        <Heading as="h3" variant="section-title">
+                            Work
                         </Heading>
-                        <p>Full-stack developer</p>
-                    </Box>
-                    <Box flexShrink={0} mt={{base:4, md:0}} ml={{md:6}} align="center">
-                        <Image 
-                            borderColor="whiteAlpha.800" 
-                            borderWidth={2} 
-                            borderStyle="solid" 
-                            maxWidth="100px" 
-                            display="inline-block" 
-                            borderRadius="full" 
-                            src="/images/mauro.jpeg" 
-                            alt="Profile Image"
-                            />
-                    </Box>
-                </Box>
-                <Section delay={0.1}>
-                    <Heading as="h3" variant="section-title">
-                        Work
-                    </Heading>
-                    <Paragraph>As a young computing enthusiast, I studied programming in a 
-                        selft-taught way since I was 12 years old,
-                        I am currently studying computer engineering and I have experience with
-                        various technologies, I've been with Javascript and Python for almost 5
-                        years, and I have developed for both backend and frontend,
-                        I am looking for new technological challenges to grow as a developer &nbsp;
-                        <NextLink href="/work/inkdrop"><Link>Inkdrop</Link></NextLink>.
-                    </Paragraph>
-                    <Box align="center" my={4}>
-                        <NextLink href="/works">
-                            <Button righIcon={<ChevronRightIcon/>} colorScheme="teal">
-                                My porfolio
-                            </Button>
-                        </NextLink>
-                    </Box>
-                </Section>
-                <Section delay={0.2}>
-                    <Heading as="h3" variant="section-title">
-                        Bio
-                    </Heading>
-                    <BioSection>
-                        <BioYear>1996</BioYear>
-                        Born in Mercedes, Uruguay.
-                    </BioSection>
-                </Section>
-            </Container> 
+                        <Paragraph>As a young computing enthusiast, I studied programming in a 
+                            selft-taught way since I was 12 years old,
+                            I am currently studying computer engineering and I have experience with
+                            various technologies, I've been with Javascript and Python for almost 5
+                            years, and I have developed for both backend and frontend,
+                            I am looking for new technological challenges to grow as a developer &nbsp;
+                            <NextLink href="/work/inkdrop"><Link>Inkdrop</Link></NextLink>.
+                        </Paragraph>
+                        <Box align="center" my={4}>
+                            <NextLink href="/works">
+                                <Button righIcon={<ChevronRightIcon/>} colorScheme="teal">
+                                    My porfolio
+                                </Button>
+                            </NextLink>
+                        </Box>
+                    </Section>
+                    <Section delay={0.2}>
+                        <Heading as="h3" variant="section-title">
+                            Bio
+                        </Heading>
+                        <BioSection>
+                            <BioYear>1996</BioYear>
+                            Born in Mercedes, Uruguay.
+                        </BioSection>
+                    </Section>
+                </Container>
+            </Layout>
 }
 
 export default Page 
--- pages/works.js
+++ pages/works.js
@@ -1,28 +1,30 @@
 import { Container, Box, Heading, SimpleGrid, Divider } from "@chakra-ui/react"
 import Section from '../components/section'
 import { WorkGridItem } from "../components/grid-item"
-
+import Layout from "../components/layouts/article";
 import thumbInkdrop from "../public/images/mauro.jpeg";
 
 const Works = () => {
     return(
-        <Container>
-            <Heading as="h3" fontSize={20} mb={4}>
-                Works
-            </Heading>
-            <SimpleGrid columns={[1,1,2]} gap={6}>
-                <Section>
-                    <WorkGridItem id="inkdrop" title="Inkdrop" thumbnail={thumbInkdrop}>
-                        A markdown note-talking app with 100+ encrypted data sync support
-                    </WorkGridItem>
-                </Section>
-                <Section>
-                    <WorkGridItem id="walkdrop" title="walkdrop" thumbnail={thumbInkdrop}>
-                        A markdown note-talking app with 100+ encrypted data sync support
-                    </WorkGridItem>
-                </Section>
-            </SimpleGrid>
-        </Container>
+        <Layout>
+            <Container>
+                <Heading as="h3" fontSize={20} mb={4}>
+                    Works
+                </Heading>
+                <SimpleGrid columns={[1,1,2]} gap={6}>
+                    <Section>
+                        <WorkGridItem id="inkdrop" title="Inkdrop" thumbnail={thumbInkdrop}>
+                            A markdown note-talking app with 100+ encrypted data sync support
+                        </WorkGridItem>
+                    </Section>
+                    <Section>
+                        <WorkGridItem id="walkdrop" title="walkdrop" thumbnail={thumbInkdrop}>
+                            A markdown note-talking app with 100+ encrypted data sync support
+                        </WorkGridItem>
+                    </Section>
+                </SimpleGrid>
+            </Container>
+        </Layout>
     )
 }
 
