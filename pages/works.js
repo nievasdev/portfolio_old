778b3a9b28e873a493c8179cb5509c216500deaf
Mauro-js <niev.mauro@gmail.com>
2022-10-29 23:05:20 -0300

    adiero imagenes

--- pages/works.js
+++ pages/works.js
@@ -2,7 +2,6 @@ import { Container, Heading, SimpleGrid } from "@chakra-ui/react"
 import Section from '../components/section'
 import { WorkGridItem } from "../components/grid-item"
 import Layout from "../components/layouts/article";
-import thumbInkdrop from "../public/images/mauro.jpeg";
 import novatech from "../public/images/novatech.png"
 
 const Works = () => {
@@ -16,11 +15,6 @@ const Works = () => {
                     <Section>
                         <WorkGridItem id="novatech" title="Novatech" thumbnail={novatech}>
                             A web page for a Sftware Factory 
-                        </WorkGridItem>
-                    </Section>
-                    <Section>
-                        <WorkGridItem id="walkdrop" title="walkdrop" thumbnail={thumbInkdrop}>
-                            A markdown note-talking app with 100+ encrypted data sync support
                         </WorkGridItem>
                     </Section>
                 </SimpleGrid>
--- pages/works/novatech.js
+++ pages/works/novatech.js
@@ -1,39 +1,42 @@
 import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
-import { Title, Meta } from '../../components/work'
+import { Title, Meta, WorkImage } from '../../components/work'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
+import novatechImage from '../../public/images/novatech.png'
+import novatechImage2 from '../../public/images/novatech2.png'
+import novatechImage3 from '../../public/images/novatech3.png'
 
 const Work = () => {
     return (
-        <Layout title="Inkdrop">
+        <Layout title="Novatech">
             <Container>
                 <Title>
                     Novatech <Badge>2022</Badge>
                 </Title>
                 <P>
-                    A bla bla bla bla bla
+                    The more dificult whit this page was build that the particles in the background,
+                    I needet remake a particular solution for the compatibility whit the current react version
                 </P>
                 <List ml={4} my={4}>
                     <ListItem>
                         <Meta>Website</Meta>
-                        <Link href='www.google.com'>
-                            Google <ExternalLinkIcon mx="2px" />
+                        <Link href='https://www.novatech.com.uy'>
+                            Novatech <ExternalLinkIcon mx="2px" />
                         </Link>
                     </ListItem>
                     <ListItem>
                         <Meta>Plataform</Meta>
-                        <span>Windows/macOs/Linux/iOs</span>
+                        <span>Web</span>
                     </ListItem>
                     <ListItem>
                         <Meta>Stack</Meta>
-                        <span>lalala, lalalal, lalalal</span>
-                    </ListItem>
-                    <ListItem>
-                        <Meta>Blogspot</Meta>
-                        <Link href='www.google.com'>Holaaa</Link>
+                        <span>React, Nodemailer, Tsparticles</span>
                     </ListItem>
                 </List>
+                <WorkImage src={novatechImage.src} alt="Novatech" />
+                <WorkImage src={novatechImage2.src} alt="Novatech2" />
+                <WorkImage src={novatechImage3.src} alt="Novatech3" />
             </Container>
         </Layout>
     )
