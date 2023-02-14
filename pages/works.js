281fee05fb4c008a6e58de3ed274abd99b72692f
Mauro-js <niev.mauro@gmail.com>
2023-02-14 11:07:23 +0000

    delete old works

--- pages/works.js
+++ pages/works.js
@@ -2,19 +2,22 @@ import { Container, Heading, SimpleGrid } from "@chakra-ui/react"
 import Section from '../components/section'
 import { WorkGridItem } from "../components/grid-item"
 import Layout from "../components/layouts/article";
-import novatech from "../public/images/novatech.png"
+import novatech from "../public/images/novatech.png";
+import hacknoid from "../public/images/hacknoid1.jpg";
 
 const Works = () => {
     return(
         <Layout>
             <Container>
-                <Heading as="h3" fontSize={20} mb={4}>
-                    Old works
-                </Heading>
                 <SimpleGrid columns={[1,1,2]} gap={6}>
                     <Section>
                         <WorkGridItem id="novatech" title="Novatech" thumbnail={novatech}>
                             A web page for a Sftware Factory 
+                        </WorkGridItem>
+                    </Section>
+                    <Section>
+                        <WorkGridItem id="hacknoid" title="Hacknoid" thumbnail={hacknoid}>
+                            Automated IT security audit service 
                         </WorkGridItem>
                     </Section>
                 </SimpleGrid>
