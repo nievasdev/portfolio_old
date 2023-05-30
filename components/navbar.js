4366eb6ae0c4fda895678c067a7658bfb99825e3
Mauro-js <niev.mauro@gmail.com>
2023-05-30 02:46:33 +0000

    add source

--- components/navbar.js
+++ components/navbar.js
@@ -1,107 +1,126 @@
-import Logo from './logo';
-import NextLink from 'next/link';
+import Logo from './logo'
+import NextLink from 'next/link'
 import {
-    Container,
-    Box,
-    Link,
-    Stack,
-    Heading,
-    Flex,
-    Menu,
-    MenuItem,
-    MenuList,
-    MenuButton,
-    IconButton,
-    useColorModeValue
-} from '@chakra-ui/react';
-import { HamburgerIcon } from '@chakra-ui/icons';
-import ThemeToggleButton from './theme-toggle-button';
+  Container,
+  Box,
+  Link,
+  Stack,
+  Heading,
+  Flex,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuButton,
+  IconButton,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { IoLogoGithub } from 'react-icons/io5'
+import { HamburgerIcon } from '@chakra-ui/icons'
+import ThemeToggleButton from './theme-toggle-button'
 
-const LinkItems = ({href, path, children}) => {
-    const active =  path === href;
-    const inactiveColor = useColorModeValue('gray200', 'whiteApha.900');  
+const LinkItems = ({ href, path, children }) => {
+  const active = path === href
+  const inactiveColor = useColorModeValue('gray200', 'whiteApha.900')
 
-    return (
-        <NextLink href={href}>
-            <Link 
-                p={2} 
-                bg={active ? 'grassTeal' : undefined }
-                color={active ? '#202023' : inactiveColor}>
-                {children}         
-            </Link>
-        </NextLink>
-    )
+  return (
+    <NextLink href={href}>
+      <Link
+        p={2}
+        bg={active ? 'grassTeal' : undefined}
+        color={active ? '#202023' : inactiveColor}
+      >
+        {children}
+      </Link>
+    </NextLink>
+  )
 }
 
 const Navbar = props => {
-     const { path } = props;
+  const { path } = props
 
-     return <Box 
-                position="fixed"
-                as="nav"
-                w="100%"
-                bg={useColorModeValue('#ffffff40', '#20202380')}
-                style={{backdropFilter : 'blur(10px'}}
-                zIndex={1}
-                {...props}
+  const inactiveColor = useColorModeValue('gray200', 'whiteApha.900')
+
+  return (
+    <Box
+      position="fixed"
+      as="nav"
+      w="100%"
+      bg={useColorModeValue('#ffffff40', '#20202380')}
+      style={{ backdropFilter: 'blur(10px' }}
+      zIndex={1}
+      {...props}
+    >
+      <Container
+        display="flex"
+        p={2}
+        maxW="container.md"
+        wrap="wrap"
+        align="center"
+        justify="space-between"
+      >
+        <Flex align="center" mr={5}>
+          <Heading as="h1" size="lg" letterSpacing={'tighter'}>
+            <Logo />
+          </Heading>
+        </Flex>
+        <Stack
+          direction={{ base: 'column', md: 'row' }}
+          display={{ base: 'none', md: 'flex' }}
+          width={{ base: 'full', md: 'auto' }}
+          alignItems="center"
+          flexGrow={1}
+          mt={{ base: 4, nmd: 0 }}
+        >
+          <LinkItems href="/works" path={path}>
+            Works
+          </LinkItems>
+          <LinkItems href="/projects" path={path}>
+            Projects
+          </LinkItems>
+          <Link
+            target="_blank"
+            href="https://github.com/Mauro-js/portfolio"
+            bg={undefined}
+            color={inactiveColor}
+            align="rigth"
+            flex={1}
+          >
+            <IoLogoGithub /> Source
+          </Link>
+        </Stack>
+        <Box flex={1} align="rigth">
+          <ThemeToggleButton />
+          <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                icon={<HamburgerIcon />}
+                variant="outline"
+                aria-label="Options"
+              />
+              <MenuList>
+                <NextLink href="/" passHref>
+                  <MenuItem as={Link}>About</MenuItem>
+                </NextLink>
+                <NextLink href="/works" passHref>
+                  <MenuItem as={Link}>Works</MenuItem>
+                </NextLink>
+                <NextLink href="/projects" passHref>
+                  <MenuItem as={Link}>Projects</MenuItem>
+                </NextLink>
+                <NextLink
+                  as={Link}
+                  href="https://github.com/Mauro-js/portfolio"
                 >
-                <Container 
-                    display="flex" 
-                    p={2} 
-                    maxW="container.md" 
-                    wrap="wrap" 
-                    align="center" 
-                    justify="space-between">
-                        <Flex align='center' mr={5}>
-                            <Heading
-                                as="h1"
-                                size="lg"
-                                letterSpacing={'tighter'}
-                            >
-                                <Logo/>
-                            </Heading>
-                        </Flex>
-                        <Stack
-                            direction={{base:'column', md:'row'}}
-                            display={{base:'none', md:'flex'}}
-                            width={{base:'full', md:'auto'}}
-                            alignItems="center"
-                            flexGrow={1}
-                            mt={{base:4,nmd:0}}
-                        >
-                            <LinkItems href="/works" path={path}>
-                                Works
-                            </LinkItems>
-                            <LinkItems href="/projects" path={path}>
-                                Projects
-                            </LinkItems>
-                        </Stack>
-                        <Box flex={1} align="rigth">
-                            <ThemeToggleButton/>
-                            <Box ml={2} display={{base:'inline-block', md:'none'}}>
-                                <Menu>
-                                     <MenuButton 
-                                        as={IconButton}
-                                        icon={<HamburgerIcon />}
-                                        variant="outline"
-                                        aria-label='Options'
-                                     />
-                                     <MenuList>
-                                        <NextLink href="/" passHref>
-                                            <MenuItem as={Link}>About</MenuItem>
-                                        </NextLink>
-                                        <NextLink href="/works" passHref>
-                                            <MenuItem as={Link}>Works</MenuItem>
-                                        </NextLink>
-                                        <NextLink href="/projects" passHref>
-                                            <MenuItem as={Link}>Projects</MenuItem>
-                                        </NextLink>
-                                     </MenuList>
-                                </Menu>
-                            </Box>
-                        </Box>
-                </Container>
-            </Box>
+                  <MenuItem as={Link}> View Source</MenuItem>
+                </NextLink>
+              </MenuList>
+            </Menu>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  )
 }
 
 export default Navbar
